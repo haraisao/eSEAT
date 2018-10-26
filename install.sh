a5ece0596c68cc818ebb575faa96e22ac1af1ebd
Isao Hara <haraisao@gmail.com>
2018-10-26 10:22:28 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,19 @@
 
 INST_DIR=/usr/local/eSEAT
 MKDIR="/bin/mkdir -p "
-CP="/bin/cp "
+CP="/usr/bin/sudo /bin/cp "
 #FILES="seatml.xsd rtc.conf"
 FILES="rtc.conf"
 DIRS="html libs 3rd_party examples bin ros_samples"
+
+echo "Install Python2"
+sudo apt-get install python-pip
+sudo apt-get install python-tk
+sudo apt-get install python-yaml
+sudo apt-get install python-lxml
+
+echo "Install eSEAT"
+sudo python setup.py install
 
 [ -d $INST_DIR ] || $MKDIR $INST_DIR
 
